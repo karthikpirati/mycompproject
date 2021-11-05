a769eeaa447f0ff54a2e9cef4ca4b8c6bbf1dd23
karthik pirati <karthikpirati@gmail.com>
2021-11-06 00:29:06 +0530

    firstcommit

---
+++ src/main/java/com/gitrepo/gitrepoproject/GitrepoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gitrepo.gitrepoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitrepoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitrepoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitrepo/gitrepoproject/GitrepoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrepo.gitrepoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitrepoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
